66509d210b6f6e26556f6d2f051b639a11168c7e
oeduardodev <duduleite0@gmail.com>
2025-03-09 00:48:27 -0300

    FEAT: Dados Primitivos e Dados Não primitivos.

--- TiposDeDados/src/Main.java
+++ TiposDeDados/src/Main.java
@@ -4,24 +4,37 @@ public class Main {
     public static void main(String[] args) {
         
         /*
-         * Criação do Ninja
+         * Dados primitivos
+         *
+         * */
+        int age = 16;
+        char rank = 'S';
+        long money = 50000000000L;
+        boolean live = true;
+
+        /*
+         * Dados não primitivos
          *
          * */
         String name = "Naruto";
-        int age = 16;
+        String nameUpperCaseFistLetra = name.substring(0, 1).toUpperCase() + name.substring(1);
         String level = "Genin";
         String cla = "Uzumaki";
+        String claUpperCase = cla.toUpperCase();
         String[] jutsus = {"clone das sombras", "rasenga", "Invocação"};
-        char rank = 'S';
-        long money = 50000000000L;
 
-        System.out.println("Name: " + name + " " + name.getClass().getSimpleName());
+        /*
+         * Imprime dados
+         *
+         * */
+        System.out.println("Name: " + nameUpperCaseFistLetra + " " + nameUpperCaseFistLetra.getClass().getSimpleName());
         System.out.println("age: " + age + " " + ((Object) age).getClass().getSimpleName());
         System.out.println("level: " + level + " " + level.getClass().getSimpleName());
-        System.out.println("cla: " + cla + " " + cla.getClass().getSimpleName());
+        System.out.println("cla: " + claUpperCase + " " + claUpperCase.getClass().getSimpleName());
         System.out.println("jutsus: " + Arrays.toString(jutsus) + " " + jutsus.getClass().getSimpleName());
         System.out.println("rank: " + rank + " " + ((Object) rank).getClass().getSimpleName());
         System.out.println("money: " + money + " " + ((Object) money).getClass().getSimpleName());
+        System.out.println("live: " + live + " " + ((Object) live).getClass().getSimpleName());
 
     }
 }
