b779a0cffe7a02c19b0b116c1777c0470136c017
oeduardodev <duduleite0@gmail.com>
2025-03-21 18:00:35 -0300

    FEAT: Objetos de Memoria

---
+++ curso/src/ObjetosDeMemoria/objetosDeMemoria.java
@@ -0,0 +1,18 @@
+package ObjetosDeMemoria;
+
+public class objetosDeMemoria {
+    public static void main(String[] args) {
+        String[] arrayString = new String[10];
+        System.out.println(arrayString[0]);
+
+        int[] arrayInt = new int[2];
+        System.out.println(arrayInt[1]);
+
+        float[] arrayFloat = new float[2];
+        System.out.println(arrayFloat[1]);
+
+        boolean[] arrayBool = new boolean[2];
+        System.out.println(arrayBool[1]);
+
+    }
+}
